385e7602a5374aa17222307e6385e3cc65752d50
bazsa3333 <bazsa3333@gmail.com>
2017-06-26 12:19:42 -0400

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Németh Bálint on 2017. 06. 26..
+//  Copyright © 2017. Németh Bálint. All rights reserved.
+//
+
+//UIKit is including Foundation
+import UIKit
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0

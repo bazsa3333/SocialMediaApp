363149fdfd3c716aee230db8f42d9b075ddfee50
bazsa3333 <bazsa3333@gmail.com>
2017-06-30 16:14:30 -0400

    Completed feed-ui

---
+++ devslopes-social/CircleView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleView.swift
+//  devslopes-social
+//
+//  Created by Németh Bálint on 2017. 06. 30..
+//  Copyright © 2017. Németh Bálint. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+}

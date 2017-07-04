f0d5e00f65bf36de0ca4d430f95b98ecae9a5084
bazsa3333 <bazsa3333@gmail.com>
2017-07-03 20:07:25 -0400

    Completed feed-table

---
+++ devslopes-social/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by Németh Bálint on 2017. 07. 03..
+//  Copyright © 2017. Németh Bálint. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+}

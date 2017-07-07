ea8347f0ec0191e78835712d9fff9c682364977c
bazsa3333 <bazsa3333@gmail.com>
2017-07-07 16:12:42 -0400

    Completed Firebase display

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -54,9 +54,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("BALINT: \(post.caption)")
-        
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+//        print("BALINT: \(post.caption)")
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            
+            cell.configureCell(post: post)
+            return cell
+        }
+        else {
+            return PostCell()
+        }
     }
     
     @IBAction func signOutTapped(_ sender: Any) {

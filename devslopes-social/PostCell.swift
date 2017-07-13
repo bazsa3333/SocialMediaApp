a281243fc620746dd9c6a579e59805ccc688ae54
bazsa3333 <bazsa3333@gmail.com>
2017-07-13 16:17:51 -0400

    Complete Firebase storage download

--- devslopes-social/PostCell.swift
+++ devslopes-social/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -22,11 +23,39 @@ class PostCell: UITableViewCell {
         super.awakeFromNib()
     }
     
-    func configureCell(post: Post) {
+    //img is optional, beacuse it isn't cached yet
+    func configureCell(post: Post, img: UIImage? = nil) {
         
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        //if it's in the cache
+        //Nagyon fontos mert ha a cache-ben van nem tölti le mégegyszer ugyan azt a képet!!!
+        if img != nil {
+            
+            self.postImg.image = img
+            //If not, we need to download it
+        } else {
+            
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 3 * 1024 * 1024, completion: { (data, error) in
+                
+                if error != nil {
+                    
+                    print("BALINT: Unabled download image from Firebase storage")
+                } else {
+                    
+                    print("BALINT: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
         
     }
 
